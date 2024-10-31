2908de47e6d697c6e66267318c15bfab763a2d32
Ronnie Nishimura Princi <rprinci@gmail.com>
2024-10-31 11:54:30 +0100

    feat: create controllers and use-cases

---
+++ src/use-cases/register.ts
@@ -0,0 +1,36 @@
+import { prisma } from "@/lib/prisma"
+import { hash } from "bcryptjs"
+
+interface RegisterRequest {
+  name: string,
+  email: string,
+  password: string,
+}
+
+export async function registerUseCase({ 
+  name, 
+  email, 
+  password 
+}: RegisterRequest) {  
+  
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    }
+  })
+
+  if (userWithSameEmail) {
+    throw new Error('E-mail already exists.')
+  }
+
+  const password_hash = await hash(password, 6)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash,
+    },
+  })
+
+}
